d9c65db4f996ba5df31f095670952cc040981dc6
Ryuseii <Ryuseii>
2017-05-24 22:23:38 +0800

    create user

---
+++ application/views/create_user_view.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+  <title>Create User</title>
+</head>
+</body>
+  <form action="<?php echo base_url();?>/createuser" method="post"
+>
+  Name = <input type = 'text' name='field1'>
+  Address = <input type = 'text' name='field2'>
+  Birthday = <input type = 'text' name='field3'>
+  Username = <input type = 'text' name='field4'>
+  Password = <input type = 'text' name='field5'>
+  Email = <input type = 'text' name='field6'>
+  <input type='submit'>
+</body>
+</html>
